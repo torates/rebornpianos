8500fd0f2d90af28808519506d12c077c0755cef
Andrés Mateo <59488791+torates@users.noreply.github.com>
2020-03-20 15:23:08 +0100

    Created script.js for the landing page

---
+++ script.js
@@ -0,0 +1,7 @@
+var bleep = new Audio();
+bleep.src = 'https://sndup.net/7vqj/audio.mp3';
+// A BMaj7 arpeggio!
+
+logotext.addEventListener ("click", function() {
+  logotext.style.animation = "foo";
+});
